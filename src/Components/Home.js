9f8b419398316d8619c1104dd5ab64bea0b36a75
soroushshalali <soroushshalali@gmail.com>
2020-04-08 11:25:59 +0200

    v0.1

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+
+class Home extends Component {
+    state = {}
+    render() {
+        return (
+            <main>
+                <h1>Hi</h1>
+            </main>
+        );
+    }
+}
+
+export default Home;
